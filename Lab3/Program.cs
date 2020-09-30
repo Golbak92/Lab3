67a5300b4f97122a3ff3ca2e455a1fa9196ca8d0
Kristian Golba <kristian.golba@iths.se>
2020-09-30 15:56:38 +0200

    .

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Lab3
 {
@@ -6,7 +7,27 @@ namespace Lab3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var fs = new FileStream(@"C:\Users\KristianGolba\Pictures\Saved Pictures\bWyA9.png", FileMode.Open);
+            var fileSize = (int)fs.Length;
+            var data = new byte[fileSize];
+            fs.Read(data, 0, fileSize);
+            fs.Close();
+
+            PNGChecker(data);
+        }
+
+        static bool PNGChecker(byte[] data)
+        {
+            var pngFile = new byte[] { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A };
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (data[i] == pngFile.Length)
+                {
+                    Console.WriteLine("This is a PNG file");
+                }
+            }
+            return true;
         }
     }
 }
